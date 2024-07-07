eb22e20c04dc04196cffccac603f89155cbd5ac3
Amitos <amit.osman@gmail.com>
2024-07-07 17:33:42 +0300

    css fix

---
+++ .history/apps/mail/cmps/mail-aside_20240707162238.jsx
@@ -0,0 +1,41 @@
+const { useState, useEffect } = React
+const { Link } = ReactRouterDOM
+
+export function MailAside({ onToggleCompose }) {
+  const filterNames = [
+    'inbox',
+    'starred',
+    'snoozed',
+    'important',
+    'sent',
+    'draft',
+  ]
+
+  return (
+    <aside className="mail-aside">
+      <div
+        className="box-compose flex align-center"
+        onClick={() => onToggleCompose()}
+      >
+        <img
+          className="box-icon"
+          src={`../../../assets/img/icon/compose-icon.png`}
+          alt=""
+        />
+        <h3>Compose</h3>
+      </div>
+
+      {filterNames.map((name) => (
+        <Link to={`/mail/${name}`} key={name} className="aside-filter flex ">
+          <div className="box-icon">
+            <img src={`../../../assets/img/icon/${name}-icon.png`} alt="" />
+          </div>
+          <div className="aside-filter-names flex space-between">
+            <div className="filter-by">{`${name}`}</div>
+            <div className="filter-by-count">5</div>
+          </div>
+        </Link>
+      ))}
+    </aside>
+  )
+}
---
+++ .history/apps/mail/cmps/mail-header_20240707173154.jsx
@@ -0,0 +1,19 @@
+import { FilterBySearch } from '../cmps/filter-by-search.jsx'
+import { FilterBySearch } from '../../../assets/img/icon'
+
+export function MailHeader({ filterBy, onSetFilterBy }) {
+  return (
+    <section className="mail-header flex align-center">
+      <div className="container-logo-hamburger flex align-center ">
+        <div className="mail-hamburger">=</div>
+        <div className="mail-logo">
+          <img src={`../../../assets/img/icon/logo-mail.png`} alt="" />
+        </div>
+      </div>
+      <div className="container-search-menu flex align-center">
+        <FilterBySearch filterBy={filterBy} onSetFilterBy={onSetFilterBy} />
+        <div className="box-menu">❖</div>
+      </div>
+    </section>
+  )
+}
